48fc3f2c1b56385aa463d7dd8ce3fe5dd4601a2c
Sikander648 <73981317+Sikander648@users.noreply.github.com>
2022-10-03 19:28:53 +0500

    one sample test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,15 @@
 <?php
 
-test('example', function () {
-    $this->assertStatus(true);
-});
+use App\User;
+use App\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
 
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    Project::factory()->create([
+        'title' => 'title',
+        'description' => 'description'
+    ]);
+});
