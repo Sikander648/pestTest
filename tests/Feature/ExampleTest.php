b765e7a3a8033dc9c17dcd9d57a075181bacdbab
Sikander648 <73981317+Sikander648@users.noreply.github.com>
2022-10-05 18:43:46 +0500

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -69,12 +69,19 @@ test('sanctum-csrf-cookie', function () {
 
 });
 
+test('register a user', function () {
+    $user = User::factory()->create();
+
+    $response = $this->postJson('/api/register-user',$user);
+
+    $response->assertDatabaseHas('user', $user);
+});
+
 test('login a user', function () {
     $user = User::factory()->create();
 
-    $response = $this->postJson('/api/login-test', ['email' => $user->email,'password' => $user->password ]);
+    $response = $this->postJson('/api/login-test', ['email' => $user->email, 'password' => $user->password]);
     $response->assertStatus(201)->assertJson(['message' => 'User is Loged In']);
-    $this->assertDatabaseHas('user', $user);
 });
 
 test('test senctum', function () {
