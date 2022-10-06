7860faa4dd7623e7fa650af51c311ee443f67471
Sikander648 <73981317+Sikander648@users.noreply.github.com>
2022-10-06 19:06:43 +0500

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -132,4 +132,17 @@ test('a user can create a project', function () {
 });
 
 
+test('a user can create a project test with headers', function () {
+    $response = $this->withHeaders([
+        'XSRF-TOKEN' => 'eyJpdiI6InpxK3RPaGhCQURZUFdqYjRNVk5qbGc9PSIsInZhbHVlIjoiUE5uZWExaER6T2t6bFFRMkhJeU9EdEhZSlFKTlh1NzZOcnp1bEtuTWtRUW8rc2laaFV4WnRFY1R6bloyM3hwMVBidHIwMCtkaHRCVVFMbnNhTG1UcUEzZ0FRdXM4VGRiOEJpQWZsa2lFeW1zWGJMRjNpc3NPVEdibXBJUFFnVXYiLCJtYWMiOiIzMmJiY2Y3YTNjYmFmNGE4OTE1MmY5N2ViMTc3NTE1MThjMDIxM2Y5ODgzMzFjNzFlMzJkNWUzYzM2NWU0ZmRhIiwidGFnIjoiIn0%3D;',
+    ])->post('/api/create-project', [
+        'title' => 'Sally',
+        'description' => 'description',
+        'owner_id' => 1,
+    ]);
+
+    $response->assertStatus(201);
+});
+
+
 
