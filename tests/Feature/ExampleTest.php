21e470b3daa2dfaeceb2271164b62e9f162c01df
Sikander648 <73981317+Sikander648@users.noreply.github.com>
2022-10-07 13:01:05 +0500

    test data type

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -72,7 +72,7 @@ test('sanctum-csrf-cookie', function () {
 test('register a user', function () {
     $user = User::factory()->create();
 
-    $response = $this->postJson('/api/register-user',$user);
+    $response = $this->postJson('/api/register-user', $user);
 
     $response->assertDatabaseHas('user', $user);
 });
@@ -95,54 +95,69 @@ test('test senctum', function () {
 });
 
 test('test senctum', function () {
-        Sanctum::actingAs(
-            User::factory()->create(),
-            ['view-tasks']
-        );
-        $this->getJson('/api/task')
+    Sanctum::actingAs(
+        User::factory()->create(),
+        ['view-tasks']
+    );
+    $this->getJson('/api/task')
         ->assertOk();
 });
 
 
-test('a user can view a project', function () {
-    Sanctum::actingAs(
-        User::factory()->create(),
-        ['view-tasks']
-    );
-    $response = $this->get('/api/show-project/1');
-    $response->assertStatus(Response::HTTP_OK);
+    test('a user can view a project', function () {
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['view-tasks']
+        );
+        $response = $this->get('/api/show-project/1');
+        $response->assertStatus(Response::HTTP_OK);
 
-});
+    });
 
-test('a user can create a project', function () {
-    Sanctum::actingAs(
-        User::factory()->create(),
-        ['view-tasks']
-    );
-    $response = $this->postJson('/api/create-project', ['title' => 'Sally', 'description' => 'description', 'owner_id' => 1]);
+    test('a user can create a project', function () {
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['view-tasks']
+        );
+        $response = $this->postJson('/api/create-project', ['title' => 'Sally', 'description' => 'description', 'owner_id' => 1]);
 
-    $response
-        ->assertStatus(201)
-        ->assertJson([
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'title' => 'Sally',
+                'description' => 'description',
+                'owner_id' => 1,
+            ]);
+
+    });
+
+
+    test('a user can create a project test with headers', function () {
+        $response = $this->withHeaders([
+            'XSRF-TOKEN' => 'eyJpdiI6InpxK3RPaGhCQURZUFdqYjRNVk5qbGc9PSIsInZhbHVlIjoiUE5uZWExaER6T2t6bFFRMkhJeU9EdEhZSlFKTlh1NzZOcnp1bEtuTWtRUW8rc2laaFV4WnRFY1R6bloyM3hwMVBidHIwMCtkaHRCVVFMbnNhTG1UcUEzZ0FRdXM4VGRiOEJpQWZsa2lFeW1zWGJMRjNpc3NPVEdibXBJUFFnVXYiLCJtYWMiOiIzMmJiY2Y3YTNjYmFmNGE4OTE1MmY5N2ViMTc3NTE1MThjMDIxM2Y5ODgzMzFjNzFlMzJkNWUzYzM2NWU0ZmRhIiwidGFnIjoiIn0%3D;',
+        ])->post('/api/create-project', [
             'title' => 'Sally',
             'description' => 'description',
             'owner_id' => 1,
         ]);
 
-});
+        $response->assertStatus(201);
+    });
 
-
-test('a user can create a project test with headers', function () {
-    $response = $this->withHeaders([
-        'XSRF-TOKEN' => 'eyJpdiI6InpxK3RPaGhCQURZUFdqYjRNVk5qbGc9PSIsInZhbHVlIjoiUE5uZWExaER6T2t6bFFRMkhJeU9EdEhZSlFKTlh1NzZOcnp1bEtuTWtRUW8rc2laaFV4WnRFY1R6bloyM3hwMVBidHIwMCtkaHRCVVFMbnNhTG1UcUEzZ0FRdXM4VGRiOEJpQWZsa2lFeW1zWGJMRjNpc3NPVEdibXBJUFFnVXYiLCJtYWMiOiIzMmJiY2Y3YTNjYmFmNGE4OTE1MmY5N2ViMTc3NTE1MThjMDIxM2Y5ODgzMzFjNzFlMzJkNWUzYzM2NWU0ZmRhIiwidGFnIjoiIn0%3D;',
-    ])->post('/api/create-project', [
-        'title' => 'Sally',
-        'description' => 'description',
-        'owner_id' => 1,
-    ]);
-
-    $response->assertStatus(201);
-});
+    it('can test data types', function () {
+        $this->withOutExceptionHandling();
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['view-tasks']
+        );
+        $project = Project::factory()->create();
+        $response = $this->postJson('/api/create-project', ['title' => $project->title, 'description' => $project->description, 'owner_id' => $project->owner_id]);
+        $response->assertJson(fn(AssertableJson $json) => $json->whereAllType([
+            'title' => 'string',
+            'owner_id' => 'integer',
+            'description' => 'string'
+        ]));
+    });
 
 
 
